1323649be33e5d6635aadb3556a134930c5e2a27
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-18 20:35:47 +0530

    React.createElement(), ReactDOM.createRoot(), root.render() and Nested React Elements

--- Session-1/App.js
+++ Session-1/App.js
@@ -1,9 +1,9 @@
-const heading = React.createElement('h1', { id: 'title' }, 'Heading 1'); // 'React' here is a global variable which comes from our injected React.js files (CDN files)
+const heading1 = React.createElement('h1', { id: 'title' }, 'Heading 1'); // 'React' here is a global variable which comes from our injected React.js files (CDN files)
 
 const heading2 = React.createElement('h2', { id: 'title' }, 'Heading 2');
 
 const container = React.createElement('div', { id: 'container' }, [
-  heading,
+  heading1,
   heading2,
 ]);
 

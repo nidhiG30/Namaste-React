2b94a98ee6f9245424efed4db50686683fc57eb7
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-21 01:45:32 +0530

    Ignited our app with NPM package manager and Parcel

--- Session-2/App.js
+++ Session-2/App.js
@@ -1,4 +1,7 @@
-const heading1 = React.createElement('h1', { id: 'title' }, 'Heading 1');
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement('h1', { id: 'title' }, 'Heading 1 from Parcel');
 
 const heading2 = React.createElement('h2', { id: 'title' }, 'Heading 2');
 

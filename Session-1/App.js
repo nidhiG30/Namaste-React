590bd28a044e96aa47b4016f0c0e4066b1a2a316
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-20 16:30:19 +0530

    Session 1 ended

--- Session-1/App.js
+++ Session-1/App.js
@@ -1,16 +1,17 @@
-const heading1 = React.createElement('h1', { id: 'title' }, 'Heading 1');
+// This is how we create a div as shown above using container
+      const heading1 = React.createElement('h1', { id: 'title' }, 'Heading 1');
 
-const heading2 = React.createElement('h2', { id: 'title' }, 'Heading 2');
+      const heading2 = React.createElement('h2', { id: 'title' }, 'Heading 2');
 
-const container = React.createElement('div', { id: 'container' }, [
-  heading1,
-  heading2,
-]);
-// When multiple elements are to be created inside a parent element (element in React, tag in HTML), we need to pass variables of those elements inside the array as there are multiple entries into the parent element. These child elements are already created and stored in separate variables.
+      const container = React.createElement('div', { id: 'container' }, [
+        heading1,
+        heading2,
+      ]);
+      // When multiple elements are to be created inside a parent element (element in React, tag in HTML), we need to pass variables of those elements inside the array as there are multiple entries into the parent element. These child elements are already created and stored in separate variables.
 
-console.log(container); // returns an object in the console. THUS, REACT ELEMENTS ARE JUST THE OBJECTS.
+      console.log(container); // returns an object in the console. THUS, REACT ELEMENTS ARE JUST THE OBJECTS.
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // tells what is the root element inside our app.
+      const root = ReactDOM.createRoot(document.getElementById('root')); // tells what is the root element inside our app.
 
-// passing react element inside the root:
-root.render(container);
+      // passing react element inside the root:
+      root.render(container);

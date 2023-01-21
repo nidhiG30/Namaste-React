b5a93116cecba00fc2d1a81a36b37db258e444d3
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-21 16:45:44 +0530

    React Element and JSX code

--- Session-3/App.js
+++ Session-3/App.js
@@ -1,50 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// React Element:
 const heading1 = React.createElement(
   'h1',
   { id: 'title', key: 'h1' },
-  'Heading 1 from Parcel',
+  'Namaste React',
 );
-
-const heading2 = React.createElement(
-  'h2',
-  { id: 'title', key: 'h2' },
-  'Heading 2',
-);
-
-/**
-<div cass="header">
-      <h1>Namaste React</h1>
-      <ul>
-        <li>About Us</li>
-        <li>Support</li>
-        <li>Home</li>
-      </ul>
-    </div>
-*/
-
-const container = React.createElement(
-  'div',
-  { id: 'container', hello: 'world' },
-  [
-    React.createElement(
-      'h1',
-      { id: 'title', key: 'h1' },
-      'Heading 1 from Parcel',
-    ),
-    React.createElement('ul', {}, [
-      React.createElement('li', {}, 'About Us'),
-      React.createElement('li', {}, 'Support'),
-      React.createElement('li', {}, 'Home'),
-    ]),
-  ],
-);
-// We can provide any piece of attribute in the React Element, which is called props in React, which are properties that take data in {key: value} pair
-
 console.log(heading1); // returns an object in the console. THUS, REACT ELEMENTS ARE JUST THE OBJECTS.
+
+
+// JSX code:
+const heading = (
+  <h1 id='title' key='h2'>
+    Namaste React
+  </h1>
+); // When writing multiple lines of JSX code, the code is to be wrapped in a parentheses.
+// As we pass attributes/properties here, as id we can pass className here as well. It's just that in React we write "className" instead of only "class".
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // tells what is the root element inside our app.
 
 // passing react element inside the root:
-root.render(container);
+root.render(heading);

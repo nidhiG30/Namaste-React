793710285bdc4184b36d675ba028fa578024288d
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-21 17:57:13 +0530

    Component Composition in React: Functional Component inside Functional Component

--- Session-3/App.js
+++ Session-3/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// React Element:
-const heading1 = React.createElement(
-  'h1',
-  { id: 'title', key: 'h1' },
-  'Namaste React',
+// Functional Component
+const Title = () => (
+  <h1 id='title' key='h2'>
+    Namaste React
+  </h1>
 );
+// This Title can be written in small letters as well: "const title". BUT ONLY WHEN it becomes a Functional Component then as per convention we must start from UpperCase letter.
 
-// React element inside React Functional Component
-const HeaderComponent = () => (
-  <div>
-    {heading1}
-    <h2>Namaste React Functional Component</h2>
-    <h2>This is a h2 tag</h2>
-  </div>
-);
-// We can omit "return" keyword if using arrow function syntax.
+// NOTE: While using arrow function syntax, if using return statement, then make sure to keep it in curly braces, otherwise if no return statement is used, must wrap the code in parentheses if multiple lines are there.
+
+// Component Composition: Functional Component inside React Functional Component
+const HeaderComponent = () => {
+  return (
+    <div>
+      {Title()}
+      <h2>Namaste React Functional Component</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
+// <Title/> OR {Title()} because functional components are JavaScript functions at the end.
+// Inside "{}", we can write any piece of JavaScript code, any piece refers to JavaScript Expressions only.
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // tells what is the root element inside our app.
 

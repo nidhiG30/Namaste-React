2a89bd9f7486cfe30f7817e395121ec08d37bdcb
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-21 15:33:16 +0530

    Using multiple React.createElement is cumbercome to use

--- Session-3/App.js
+++ Session-3/App.js
@@ -1,17 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const heading1 = React.createElement('h1', { id: 'title' }, 'Heading 1 from Parcel');
+const heading1 = React.createElement(
+  'h1',
+  { id: 'title', key: 'h1' },
+  'Heading 1 from Parcel',
+);
 
-const heading2 = React.createElement('h2', { id: 'title' }, 'Heading 2');
+const heading2 = React.createElement(
+  'h2',
+  { id: 'title', key: 'h2' },
+  'Heading 2',
+);
 
-const container = React.createElement('div', { id: 'container', hello: 'world' }, [
-  heading1,
-  heading2,
-]);
+/**
+<div cass="header">
+      <h1>Namaste React</h1>
+      <ul>
+        <li>About Us</li>
+        <li>Support</li>
+        <li>Home</li>
+      </ul>
+    </div>
+*/
+
+const container = React.createElement(
+  'div',
+  { id: 'container', hello: 'world' },
+  [
+    React.createElement(
+      'h1',
+      { id: 'title', key: 'h1' },
+      'Heading 1 from Parcel',
+    ),
+    React.createElement('ul', {}, [
+      React.createElement('li', {}, 'About Us'),
+      React.createElement('li', {}, 'Support'),
+      React.createElement('li', {}, 'Home'),
+    ]),
+  ],
+);
 // We can provide any piece of attribute in the React Element, which is called props in React, which are properties that take data in {key: value} pair
 
-console.log(container); // returns an object in the console. THUS, REACT ELEMENTS ARE JUST THE OBJECTS.
+console.log(heading1); // returns an object in the console. THUS, REACT ELEMENTS ARE JUST THE OBJECTS.
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // tells what is the root element inside our app.
 

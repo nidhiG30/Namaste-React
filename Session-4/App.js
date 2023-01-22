b721a82f1301de30df9b52f40c7716cb0f17b9c7
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-22 15:35:35 +0530

    Because of <a> tag, user get to redirect on the home page after clicking logo

--- Session-4/App.js
+++ Session-4/App.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 // Functional Component
 const Title = () => (
-  <h1 id='title' key='h2'>
-    Khaana
-  </h1>
+  <a href='/'>
+    <img
+      className='logo'
+      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPQFELK3a9H16QTturOu6HihNMpAp5WbqN-Q&usqp=CAU'
+      alt='logo'
+    />
+  </a>
 );
 
 // Composing Components

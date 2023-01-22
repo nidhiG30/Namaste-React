ac5ab909d27e2bd92bac58d72729a9cb0bb3db52
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-23 00:56:32 +0530

    Because of unique key, only that particular element is rendered in the DOM, and the whole page need not be rendered again : Reconciliation.

--- Session-4/App.js
+++ Session-4/App.js
@@ -904,15 +904,13 @@ const RestaurantCard = ({ name, cuisines, avgRating, cloudinaryImageId }) => {
   );
 };
 
+// no key (not acceptable) <<<<<<<< index key (last option) <<<< unique key (best practice)
 const Body = () => {
   return (
     <div className='restaurant-list'>
-      {
-        restaurantList.map(restaurant => {
-          return <RestaurantCard {...restaurant.data} key={restaurant.data.id}/> // Here the "key" remains unique for every element that gets added to the DOM, reconciliation.
-        })
-      }
-      
+      {restaurantList.map(restaurant => {
+        return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />; // Here the "key" remains unique for every element that gets added to the DOM, reconciliation.
+      })}
     </div>
   );
 };

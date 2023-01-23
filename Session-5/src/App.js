494529664b7b7ad9b562e8c49684eaa9bb21488b
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-23 16:42:41 +0530

    Created 'RestaurantCard.js' component and exported it.

--- Session-5/src/App.js
+++ Session-5/src/App.js
@@ -3,24 +3,6 @@ import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-import { IMG_CDN_URL } from './config';
-
-const RestaurantCard = ({ name, cuisines, avgRating, cloudinaryImageId }) => {
-  // destructuring the object data which we get as prop
-  return (
-    <div className='card'>
-      <img
-        src={
-          IMG_CDN_URL +
-          cloudinaryImageId
-        }
-      />
-      <h2>{name}</h2>
-      <h3>{cuisines.join(', ')}</h3>
-      <h4>{avgRating} Stars</h4>
-    </div>
-  );
-};
 
 const AppLayout = () => {
   return (

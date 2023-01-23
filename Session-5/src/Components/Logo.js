b03b7cc4219d88005509eab53df5b8d98e7766dd
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-23 13:43:32 +0530

    Default Export of a function component Logo.js into the main App.js file.

---
+++ Session-5/src/Components/Logo.js
@@ -0,0 +1,12 @@
+// Functional Component
+const Logo = () => (
+  <a href='/'>
+    <img
+      className='logo'
+      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPQFELK3a9H16QTturOu6HihNMpAp5WbqN-Q&usqp=CAU'
+      alt='logo'
+    />
+  </a>
+);
+
+export default Logo;

2193c484b0c6ee3d457169fb393ed8895fa9911d
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-24 14:53:50 +0530

    Exploring the World!

---
+++ Session-6/src/components/Body.js
@@ -0,0 +1,50 @@
+import { restaurantList } from '../config';
+import RestaurantCard from './RestaurantCard';
+import { useState } from 'react';
+
+function filterData(searchText, restaurants) {
+  return restaurants.filter(restaurant => restaurant?.data?.name.includes(searchText));
+}
+
+const Body = () => {
+ const [restaurants, setRestaurants] = useState(restaurantList);
+  const [searchText, setSearchText] = useState();
+
+  return (
+    <>
+      <div className='search-container'>
+        <input
+          type='text'
+          className='search-input'
+          placeholder='Search'
+          value={searchText}
+          onChange={e => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className='search-btn'
+          onClick={() => {
+            // need to filter the data from restaurantList
+            const data = filterData(searchText, restaurants);
+
+            // update the state - restaurants
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+
+      <div className='restaurant-list'>
+        {restaurants.map(restaurant => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          ); // Here the "key" remains unique for every element that gets added to the DOM, reconciliation.
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
---
+++ Session-6/src/components/Header.js
@@ -0,0 +1,29 @@
+// Functional Component
+const Logo = () => (
+  <a href='/'>
+    <img
+      className='logo'
+      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPQFELK3a9H16QTturOu6HihNMpAp5WbqN-Q&usqp=CAU'
+      alt='logo'
+    />
+  </a>
+);
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <Logo />
+
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

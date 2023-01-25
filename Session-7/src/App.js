dba0c1882357e7a43eed47affa857159b0bd5e4a
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-25 21:18:33 +0530

    Import createBrowserRouter from 'react-router-dom' to route to 'About Us' page upon clicking.

--- Session-7/src/App.js
+++ Session-7/src/App.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
+import { createBrowserRouter } from 'react-router-dom';
+
+
 
 const AppLayout = () => {
   return (

3a692feb33dc7955c906db03e9d0bf661dd880b4
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-25 13:40:57 +0530

    Added toLowerCase() in 'searchText' state variable. Added a condition to check if searched Restaurants are available or not.

--- Session-6/src/components/Body.js
+++ Session-6/src/components/Body.js
@@ -4,33 +4,48 @@ import { useState, useEffect } from 'react';
 import Shimmer from './Shimmer';
 
 function filterData(searchText, restaurants) {
-  return restaurants.filter(restaurant => restaurant?.data?.name.includes(searchText));
+  return restaurants.filter(restaurant =>
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase()),
+  );
 }
 
 const Body = () => {
- const [restaurants, setRestaurants] = useState([]); // initially empty list of restaurants
-  const [searchText, setSearchText] = useState(""); //initially no text in search bar
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]); // initially empty list of restaurants
+  const [searchText, setSearchText] = useState(''); //initially no text in search bar
 
   // Empty dependecy array is called only once after every render
-  // dep array [searchText] => once after initial render + render everytime the state is changed  
+  // dep array [searchText] => once after initial render + render everytime the state is changed
   useEffect(() => {
     // API call
     getRestaurants();
   }, []);
 
   async function getRestaurants() {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1189882&lng=79.04192739999999&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch(
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1189882&lng=79.04192739999999&page_type=DESKTOP_WEB_LISTING',
+    );
     const json = await data.json();
     console.log(json);
 
     // Insert real data of the restaurants on UI fetched from Swiggy's API
-    setRestaurants(json?.data?.cards[2]?.data?.data?.cards) // To put the data
-    
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards); // To put the data
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards); // To put the data
   }
 
-  console.log("render");
+  console.log('render');
 
-  return (restaurants.length === 0) ? <Shimmer /> : (
+  // Conditional Rendering
+  // if restaurant is empty (allRestaurants and filteredRestaurants both contains data, so that's why we take allRestaurants to check this condition as it provides wholesome data) => shimmer UI
+  // if restaurant has data => actual data UI
+
+  if (!allRestaurants) return null; // Early return => (when no restaurant found, component not rendered)
+
+  if (filteredRestaurants?.length === 0) return <h1>No Restaurant match your filter!!</h1>
+
+  return allRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className='search-container'>
         <input
@@ -46,10 +61,10 @@ const Body = () => {
           className='search-btn'
           onClick={() => {
             // need to filter the data from restaurantList
-            const data = filterData(searchText, restaurants);
+            const data = filterData(searchText, allRestaurants);
 
             // update the state - restaurants
-            setRestaurants(data);
+            setFilteredRestaurants(data);
           }}
         >
           Search
@@ -57,7 +72,7 @@ const Body = () => {
       </div>
 
       <div className='restaurant-list'>
-        {restaurants.map(restaurant => {
+        {filteredRestaurants.map(restaurant => {
           return (
             <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
           ); // Here the "key" remains unique for every element that gets added to the DOM, reconciliation.
@@ -68,3 +83,5 @@ const Body = () => {
 };
 
 export default Body;
+
+// 2:30

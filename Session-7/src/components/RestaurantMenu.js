930514eb4fc10af62b9f636843691bd6c5ceda28
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-27 02:54:51 +0530

    With useEffect() make an API call to fetch async data, then show/render that using useState.

--- Session-7/src/components/RestaurantMenu.js
+++ Session-7/src/components/RestaurantMenu.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import {useParams} from "react-router-dom"
+import {useParams} from "react-router-dom";
+import { IMG_CDN_URL } from "../config";
 
 const RestaurantMenu = () => {
   // How to read dynamic URL params
@@ -14,13 +15,24 @@ const RestaurantMenu = () => {
   async function getRestaurantInfo() {
     const data = await fetch("https://www.swiggy.com/dapi/menu/v4/full?lat=21.1189882&lng=79.04192739999999&menuId=359586");
     const json = await data.json();
-    console.log(json);
+    console.log(json.data);
+    setRestaurant(json.data);
   }
 
   return (
     <div>
-      <h1>Restaurant Id: {resId}</h1> {/* 'id' could be 'resId' */}
-      <h2>Namaste</h2>
+      <div>
+        <h1>Restaurant Id: {resId}</h1> {/* 'id' could be 'resId' */}
+        <h2>{restaurant.name}</h2>
+        <img src={IMG_CDN_URL + restaurant.cloudinaryImageId}/>
+        <h3>{restaurant.area}</h3>
+        <h3>{restaurant.city}</h3>
+        <h3>{restaurant.avgRating} ⭐</h3>
+        <h3>{restaurant.costForTwoMsg}</h3>
+      </div>
+      <div>
+        {console.log(Object.values(restaurant.menu.items))} {/* To checkout menu items */}
+      </div>
     </div>
   );
 };

80bcf2e93701a13355e4cc795388389e1714290a
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-31 21:43:58 +0530

    Destructuring the 'state' variable in Class Component.

--- Session-8/src/components/ProfileClass.js
+++ Session-8/src/components/ProfileClass.js
@@ -2,23 +2,24 @@ import React from 'react';
 class Profile extends React.Component {
   constructor(props) {
     super(props);
-    
+
     // Create State
     this.state = {
-        count: 0
-      }
+      count: 0,
+    };
   }
 
   render() {
+    // Destructuring the 'state'
+    const { count } = this.state;
     return (
       <div>
         <h1>Profile Class Component</h1>
-        {/* React attaches all the props from parent element to the "this" of CBC */}
         <h2>Name: {this.props.name}</h2>
         <h3>XYZ: {this.props.xyz}</h3>
 
         {/* Access the state variable */}
-        <h3>Count: {this.state.count}</h3>
+        <h3>Count: {count}</h3>
       </div>
     );
   }

f02d0f3de6b9be0ebb890dda56724e5d9c53a389
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-31 15:09:05 +0530

    Nested routing of <Profile /> comp without using <Outlet /> by importing it in its parent which is the About() comp.

--- Session-8/src/components/About.js
+++ Session-8/src/components/About.js
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import Profile from './Profile';
 
 const About = () => {
   return (
     <div>
       <h1>About Us Page</h1>
       <p>This is the Namaste React Live Course</p>
-      <Outlet />
+      <Profile />
     </div>
   );
 };

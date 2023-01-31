976c088b93ee29b466fbab0e7eee53d1e87ce036
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-01 01:48:24 +0530

    Making our app with class based components.

--- Session-8/src/components/About.js
+++ Session-8/src/components/About.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import ProfileFunctionalComponent from './Profile';
 import Profile from './ProfileClass';
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Us Page</h1>
-      <p>This is the Namaste React Live Course</p>
-      <ProfileFunctionalComponent name={"Nidhi"}/>
-      <Profile name={"Nitti Gaje"} xyz="abc" /> {/* React collects all the props for Class-Based-Components */}
-    </div>
-  );
-};
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us Page</h1>
+        <p>This is the Namaste React Live Course</p>
+        <ProfileFunctionalComponent name={"Nidhi"}/>
+        <Profile name={"Nitti Gaje"} xyz="abc" /> {/* React collects all the props for Class-Based-Components */}
+      </div>
+    );
+  }
+}
 
 export default About;

fb288c941c2ce7eff92b016fdc8b05f4e3b13b8c
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-31 15:01:08 +0530

    '<Outlet />' takes in children properties. Since the 'About' config is the parent of 'Profile' config, thus the About() component will take the <Outlet /> component from 'react-router-dom'.

--- Session-8/src/components/About.js
+++ Session-8/src/components/About.js
@@ -1,8 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
 const About = () => {
   return (
     <div>
       <h1>About Us Page</h1>
       <p>This is the Namaste React Live Course</p>
+      <Outlet />
     </div>
   );
 };

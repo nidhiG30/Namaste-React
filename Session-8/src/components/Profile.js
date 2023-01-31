9b92a2ae036e89fea99c525524de9095e296ba9f
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-31 23:54:25 +0530

    'setFunction' in Class Component Vs. Function Component.

--- Session-8/src/components/Profile.js
+++ Session-8/src/components/Profile.js
@@ -1,13 +1,14 @@
 import {useState} from 'react';
 
 const Profile = (props) => {
-  const [count] = useState(0); // creating state without setFunction().
-  const [count2] = useState(0); // One-by-one states are created in func comp
+  const [count, setCount] = useState(0); // creating state with setFunction().
+  
   return (
     <div>
       <h2>Profile Component</h2>
       <h3>Name: {props.name}</h3> {/* Using props */}
       <h3>Count: {count}</h3> {/* Using state */}
+      <button onClick={() => setCount(1)}>Click</button> {/* setFunction in Function comp */}
     </div>
   )
 }
--- Session-8/src/components/ProfileClass.js
+++ Session-8/src/components/ProfileClass.js
@@ -6,12 +6,11 @@ class Profile extends React.Component {
     // Create State
     this.state = {
       count: 0,
-      count2: 0 // Multiple states are created in single state object 'this.state'
+      count2: 0, // Multiple states are created in single state object 'this.state'
     };
   }
 
   render() {
-    // Destructuring the 'state'
     const { count } = this.state;
     return (
       <div>
@@ -19,8 +18,16 @@ class Profile extends React.Component {
         <h2>Name: {this.props.name}</h2>
         <h3>XYZ: {this.props.xyz}</h3>
 
-        {/* Access the state variable */}
         <h3>Count: {count}</h3>
+        <button
+          onClick={() => {
+            this.setState({ // setFUnction in Class Comp
+              count: 1
+            });
+          }}
+        >
+          SetCount
+        </button>
       </div>
     );
   }

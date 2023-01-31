fcd4df5ace6177e77d8227a3c71b668d5e35d049
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-31 21:34:16 +0530

    Using 'state' in functional component.

--- Session-8/src/components/Profile.js
+++ Session-8/src/components/Profile.js
@@ -1,8 +1,12 @@
+import {useState} from 'react';
+
 const Profile = (props) => {
+  const [count] = useState(0); // creating state without setFunction().
   return (
     <div>
       <h2>Profile Component</h2>
-      <h3>Name: {props.name}</h3>
+      <h3>Name: {props.name}</h3> {/* Using props */}
+      <h3>Count: {count}</h3> {/* Using state */}
     </div>
   )
 }

f86f4bc8100c934e0b30feafb2963ff137e489f7
nidhiG30 <gadgenidhi9@gmail.com>
2023-01-31 21:36:32 +0530

    Using 'state' in class-based component.

--- Session-8/src/components/ProfileClass.js
+++ Session-8/src/components/ProfileClass.js
@@ -1,5 +1,14 @@
 import React from 'react';
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    // Create State
+    this.state = {
+        count: 0
+      }
+  }
+
   render() {
     return (
       <div>
@@ -7,6 +16,9 @@ class Profile extends React.Component {
         {/* React attaches all the props from parent element to the "this" of CBC */}
         <h2>Name: {this.props.name}</h2>
         <h3>XYZ: {this.props.xyz}</h3>
+
+        {/* Access the state variable */}
+        <h3>Count: {this.state.count}</h3>
       </div>
     );
   }

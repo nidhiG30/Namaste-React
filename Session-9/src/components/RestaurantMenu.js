ec51b4e2a9da8fe2f0f13f0c1a234ba955b8ca3d
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-06 14:32:57 +0530

    'useRestaurant()' Hook is created to get data from API and and return restaurant data. This hook takes an ID which which will be different for each restaurant.

--- Session-9/src/components/RestaurantMenu.js
+++ Session-9/src/components/RestaurantMenu.js
@@ -2,12 +2,15 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { IMG_CDN_URL } from '../config';
 import Shimmer from './Shimmer';
+import useRestaurant from '../utils/useRestaurant';
 
 const RestaurantMenu = () => {
   // How to read dynamic URL params
   const { resId } = useParams();
 
-  const [restaurant, setRestaurant] = useState(null); // 'null' poses as a 'value' than just passing an 'empty object {}' which causes error on page reload
+  // const [restaurant, setRestaurant] = useState(null);
+
+  const restaurant = useRestaurant(resId);
 
   useEffect(() => {
     getRestaurantInfo();
---
+++ Session-9/src/utils/useRestaurant.js
@@ -0,0 +1,5 @@
+const useRestaurant = (resId) => {
+
+}
+
+export default useRestaurant;

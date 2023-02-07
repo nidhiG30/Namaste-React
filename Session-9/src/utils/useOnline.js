9f31d096976caac97d3c4fcfed6785999ea08a93
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-07 12:45:50 +0530

    Add event listener on Windows to get online/offine status. We need to run this logic Only ONCE, that's why used useEffect() with an empty array.

--- Session-9/src/utils/useOnline.js
+++ Session-9/src/utils/useOnline.js
@@ -1,13 +1,18 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const useOnline = () => {
   const isOnline = () => {
-    const [isOnline, setIsOnline] = useState();
-    
-  }
+    const [isOnline, setIsOnline] = useState(true);
+
+    useEffect(() => {
+      // Logic to keep online/offline status
+      window.addEventListener('online', () => {
+        
+      }); // This event listener is called only Once on page load because of empty array
+    }, []);
+  };
 
   return isOnline; // returns true or false, this will be used to show the internet status
-
 };
 
 export default useOnline;

aeb191150fb60cf122016ad11fc986b46fcd78e5
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-09 18:58:52 +0530

    Styled restaurant cards.

--- Session-10/src/components/RestaurantCard.js
+++ Session-10/src/components/RestaurantCard.js
@@ -3,14 +3,14 @@ import { IMG_CDN_URL } from '../config';
 const RestaurantCard = ({ name, cuisines, avgRating, cloudinaryImageId }) => {
 
   return (
-    <div className='card w-[200px]'>
+    <div className='w-56 p-2 m-2 shadow-lg bg-pink-50'>
       <img
         src={
           IMG_CDN_URL +
           cloudinaryImageId
         }
       />
-      <h2>{name}</h2>
+      <h2 className='font-bold text-xl'>{name}</h2>
       <h3>{cuisines.join(', ')}</h3>
       <h4>{avgRating} Stars</h4>
     </div>

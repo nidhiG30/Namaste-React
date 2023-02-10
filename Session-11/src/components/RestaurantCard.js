53c4bb1add6e7700dbb9b9119cf2e682496d9e6f
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-10 15:59:49 +0530

    'PROPS DRILLIG': Restaurant Card is in Body, and Body is in App Layout. To pass data into Restaurant card we need to follow the path from App -> Body -> Restaurant Card wherein the local states and props carry the data from one place to another. Hence, 'passing data' from Parent to Child and then to its further child of child, this process is called Props Drilling.

--- Session-11/src/components/RestaurantCard.js
+++ Session-11/src/components/RestaurantCard.js
@@ -1,6 +1,6 @@
 import { IMG_CDN_URL } from '../config';
 
-const RestaurantCard = ({ name, cuisines, avgRating, cloudinaryImageId }) => {
+const RestaurantCard = ({ name, cuisines, avgRating, cloudinaryImageId, user }) => {
 
   return (
     <div className='w-56 p-2 m-2 shadow-lg bg-pink-50'>
@@ -13,6 +13,7 @@ const RestaurantCard = ({ name, cuisines, avgRating, cloudinaryImageId }) => {
       <h2 className='font-bold text-xl'>{name}</h2>
       <h3>{cuisines.join(', ')}</h3>
       <h4>{avgRating} Stars</h4>
+      <h4>{user.name}</h4>
     </div>
   );
 };

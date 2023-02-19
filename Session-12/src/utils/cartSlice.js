2c059edf4ffe4721f399778552fc3a6911204e9d
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-20 00:02:01 +0530

    We can define multiple 'actions' inside 'reducers' whose reducer function can have logic defined as per the task/action to be performed.

--- Session-12/src/utils/cartSlice.js
+++ Session-12/src/utils/cartSlice.js
@@ -9,6 +9,9 @@ const cartSlice = createSlice({
     addItem: (state, action) => {
       state.items.push(action.payload);
     },
+    removeItem: (state, action) => {
+      state.items.pop();
+    },
     clearCart: (state) => {
       state.items = [];
     }

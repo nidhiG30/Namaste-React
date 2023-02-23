f11e47bec35ceb4a730804533a070356a65710f2
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-23 20:19:58 +0530

    FoodItems component was created to see what food items are added in the cart.

--- Session-12/src/components/Cart.js
+++ Session-12/src/components/Cart.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import FoodItem from './FoodItem';
 
 const Cart = () => {
   const cartItems = useSelector(store => store.cart.items);
@@ -6,6 +7,7 @@ const Cart = () => {
   return (
     <div>
       <h1 className='font-bold text-3xl'>Cart Items - {cartItems.length}</h1>
+      <FoodItem {...cartItems[0]}/>
     </div>
   );
 };

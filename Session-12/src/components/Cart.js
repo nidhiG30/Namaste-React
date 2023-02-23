1b0a25f77ae95f27fb203e0000edcf1047bcc070
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-23 18:46:11 +0530

    Bad Approach to subcribe to store slice. Everytime the component is updated, the entire store will be rerendered where 100s & 1000s of slices will be present and those need not be rerendered.

--- Session-12/src/components/Cart.js
+++ Session-12/src/components/Cart.js
@@ -1,7 +1,16 @@
+import { useSelector } from "react-redux";
+
 const Cart = () => {
+  
+  // const cartItems = useSelector(store => store.cart.items)
+  
+  const store = useSelector(store => store);
+
   return (
     <div>
-      <h1 className='font-bold text-3xl'>Cart Items</h1>
+      {/* <h1 className='font-bold text-3xl'>Cart Items - {cartItems.length}</h1> */}
+      
+      <h1 className='font-bold text-3xl'>Cart Items - {store.cart.items.length}</h1>
     </div>
   );
 };

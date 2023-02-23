40ebd0c934a2f3d2f6097d58298c0a19df75fc80
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-23 13:18:03 +0530

    'Dispatching an Action': On click of a button, some event should happen and that is handled by the event handler function 'handleAddItem()'. This handler will dispatch an action, and pass the payload, means, if addItem is an action, then the data that needs to be added in a database which is store is the 'payload' which is passed with the 'addItem' action.

--- Session-12/src/components/RestaurantMenu.js
+++ Session-12/src/components/RestaurantMenu.js
@@ -3,11 +3,20 @@ import { useParams } from 'react-router-dom';
 import { IMG_CDN_URL } from '../config';
 import Shimmer from './Shimmer';
 import useRestaurant from '../utils/useRestaurant';
+import { addItem } from '../utils/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
 
   const restaurant = useRestaurant(resId); // data from useRestaurant() hook goes inside restaurant.
+
+  const dispatch = useDispatch();
+
+  const handleAddItem = () => {
+    // on click of Add Item button created below this function is called
+    dispatch(addItem('Grapes')); // dispatch(action (pass the passload)).
+  };
 
   return !restaurant ? (
     <Shimmer />
@@ -21,6 +30,14 @@ const RestaurantMenu = () => {
         <h3>{restaurant?.city}</h3>
         <h3>{restaurant?.avgRating} ⭐</h3>
         <h3>{restaurant?.costForTwoMsg}</h3>
+      </div>
+      <div>
+        <button
+          className='p-2 m-5 bg-green-100'
+          onClick={() => handleAddItem()}
+        >
+          Add Item
+        </button>
       </div>
       <div>
         <h1>Menu</h1>

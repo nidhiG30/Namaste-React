fa276148003a2cf66b993e76dd8f3cee1a7e29db
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-25 01:17:22 +0530

    Our App's Header is associated with Redux Store means it is subscribed to it. Thus, actual Provider need to be given to Header component. This Provider will also take something as 'store'.

--- Session-13/src/components/__tests__/Header.test.js
+++ Session-13/src/components/__tests__/Header.test.js
@@ -1,9 +1,15 @@
 import { render } from '@testing-library/react';
 import Header from '../Header';
+import { Provider } from 'react-redux';
+import store from '../../utils/store';
 
 test('Logo should load on rendering header', () => {
   // Load Header
-  const header = render(<Header />);
+  const header = render(
+    <Provider store={store}>
+      <Header />
+    </Provider>,
+  );
   console.log(header);
 
   // Check of logo is loaded

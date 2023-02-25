fde5418e5ef209cb4d372949fb0271f3ad6c55cd
nidhiG30 <gadgenidhi9@gmail.com>
2023-02-25 23:11:53 +0530

    '{StaticRouter}' from 'react-router-dom/server' used to provide router for JSDOM. Finally with this router alongwith Provider and store, all the test cases passed.

--- Session-13/src/components/__tests__/Header.test.js
+++ Session-13/src/components/__tests__/Header.test.js
@@ -2,13 +2,16 @@ import { render } from '@testing-library/react';
 import Header from '../Header';
 import { Provider } from 'react-redux';
 import store from '../../utils/store';
+import { StaticRouter } from 'react-router-dom/server';
 
 test('Logo should load on rendering header', () => {
   // Load Header
   const header = render(
-    <Provider store={store}>
-      <Header />
-    </Provider>,
+    <StaticRouter>
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    </StaticRouter>,
   );
   console.log(header);
 

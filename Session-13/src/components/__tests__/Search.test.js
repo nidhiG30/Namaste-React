dbdd874aad3f276d2b5ea8fa4eb3a264b0ab1c4e
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-01 15:04:29 +0530

    Mocking Fetch I: Using a dummy func 'jest.fn()' provided by Jest. In the outer promise, a readable stream is returned which is then converted to JSON format in inner promise. 'TypeError: Cannot read properties of undefined (reading 'json')'.

--- Session-13/src/components/__tests__/Search.test.js
+++ Session-13/src/components/__tests__/Search.test.js
@@ -2,7 +2,14 @@ import Body from '../Body';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../../utils/store.js';
-import {StaticRouter} from 'react-router-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import { RESTAURANT_DATA } from '../../mocks/data.js';
+
+global.fetch = jest.fn(() => {
+  Promise.resolve({
+    json: Promise.resolve(RESTAURANT_DATA),
+  });
+});
 
 test('Search Results on Home Page', () => {
   const body = render(
@@ -10,8 +17,8 @@ test('Search Results on Home Page', () => {
       <Provider store={store}>
         <Body />
       </Provider>
-    </StaticRouter>
+    </StaticRouter>,
   );
-  
+
   console.log(body);
 });

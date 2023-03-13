e9f17fb2cda6aa193d5a3cd9bf7a07a03f2ec38e
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-13 23:53:26 +0530

    Developed Caching/Memoization for the search inputs/results.

--- Session-15/namaste-youtube/src/components/Head.js
+++ Session-15/namaste-youtube/src/components/Head.js
@@ -1,34 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleMenu } from '../utils/appSlice';
 import { YOUTUBE_SEARCH_API } from '../utils/constants';
+import { cacheResults } from '../utils/searchSlice';
 
 const Head = () => {
-  const [serachQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const searchCache = useSelector(store => store.search);
+  const dispatch = useDispatch();
 
   // Implemented Debouncing:
   useEffect(() => {
-    // API call
-
-    // make an API call after every key press
-    // but if the diff b/w two API calls is <200ms
-    // decline the API call
-    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [serachQuery]);
+  }, [searchQuery]);
 
   const getSearchSuggestions = async () => {
-    console.log('API Call - ' + serachQuery);
-    const data = await fetch(YOUTUBE_SEARCH_API + serachQuery);
+    console.log('API Call - ' + searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
-    // console.log(json);
+    // console.log(json[1]);
+    setSuggestions(json[1]);
+
+    // update cache
+    dispatch(cacheResults({
+      [searchQuery]: json[1],
+    }));
   };
-
-  const dispatch = useDispatch();
-
+  
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
@@ -55,18 +66,26 @@ const Head = () => {
           <input
             className='px-5 w-1/2 border border-gray-400 p-2 rounded-l-full'
             type='text'
-            value={serachQuery}
+            value={searchQuery}
             onChange={e => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
           <button className='border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100'>
             🔍
           </button>
         </div>
-        <div className='fixed bg-white py-2 px-2 w-[39rem] shadow-lg rounded-xl border border-gray-100'>
-          <ul>
-            <li className='py-2 px-2 shadow-sm hover:bg-gray-100'>🔍 Iphone</li>
-          </ul>
-        </div>
+        {showSuggestions && (
+          <div className='fixed bg-white py-2 px-2 w-[39rem] shadow-lg rounded-xl border border-gray-100'>
+            <ul>
+              {suggestions.map(s => (
+                <li key={s} className='py-2 px-2 shadow-sm hover:bg-gray-100'>
+                  {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className='col-span-1'>
         <img

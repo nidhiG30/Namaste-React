f4db33ea1630ee1c20f7281da40a43c5cc0a2256
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-15 01:09:27 +0530

    The End Game 15/03/20223.

---
+++ Session-16/namaste-youtube/src/components/ButtonList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button'
+
+const list = ["All", "Live", "Gaming", "Songs", "Soccer", "Badminton", "Cooking", "Mantras", "News", "Sadhguru", "Tourism", "Comedy", "Podcasts"];
+
+const btnItems = list.map(items => <Button name={items} key={items}/>)
+
+const ButtonList = () => {
+  return (
+    <div className='flex'>
+      {btnItems}
+    </div>
+  )
+}
+
+export default ButtonList

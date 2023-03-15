4a64bdf69c9e05d74ce0c56aab4f0db012f8c85d
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-15 22:52:11 +0530

    After every 2s, poll the data and get the data, and the next step would be to append the data for which we need to build the redux store.

--- Session-16/namaste-youtube/src/components/LiveChat.js
+++ Session-16/namaste-youtube/src/components/LiveChat.js
@@ -1,25 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ChatMessage from './ChatMessage';
 
 const LiveChat = () => {
+  useEffect(() => {
+
+    // API Polling
+    const i = setInterval(() => {
+      console.log('API Polling');
+    }, 2000)
+
+    return () => clearInterval(i);
+  }, []);
+
   return (
     <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
-      <ChatMessage
-        name='Nidhi Gadge'
-        message='This is Namaste React Live! 🙏'
-      />
-      <ChatMessage
-        name='Nidhi Gadge'
-        message='This is Namaste React Live! 🙏'
-      />
-      <ChatMessage
-        name='Nidhi Gadge'
-        message='This is Namaste React Live! 🙏'
-      />
-      <ChatMessage
-        name='Nidhi Gadge'
-        message='This is Namaste React Live! 🙏'
-      />
       <ChatMessage
         name='Nidhi Gadge'
         message='This is Namaste React Live! 🙏'

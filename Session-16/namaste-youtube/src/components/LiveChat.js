1c56f0acfe950e88a7ab936a284df940e31bdd1e
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-15 23:47:22 +0530

    Dispatching the addMessage Action after the API Polling done in the live chat section.

--- Session-16/namaste-youtube/src/components/LiveChat.js
+++ Session-16/namaste-youtube/src/components/LiveChat.js
@@ -1,12 +1,21 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addMessage } from '../utils/chatSlice';
 import ChatMessage from './ChatMessage';
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
 
     // API Polling
     const i = setInterval(() => {
       console.log('API Polling');
+
+      dispatch(addMessage({
+        name: 'Nidhi Gadge',
+        message: "Lorem Ipsum Dolor Site Amet 🚀"
+      }))
     }, 2000)
 
     return () => clearInterval(i);

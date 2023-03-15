efdb7a7b8468d46a5db425baca324624bb89620c
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-16 01:00:00 +0530

    Created a helper file to keep account of random names and their chats that should get displayed in the live chat component.

--- Session-16/namaste-youtube/src/components/LiveChat.js
+++ Session-16/namaste-youtube/src/components/LiveChat.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addMessage } from '../utils/chatSlice';
+import { generateRandomName, makeRandomMessage } from '../utils/helper';
 import ChatMessage from './ChatMessage';
 
 const LiveChat = () => {
@@ -15,8 +16,8 @@ const LiveChat = () => {
 
       dispatch(
         addMessage({
-          name: 'Nidhi Gadge',
-          message: 'Lorem Ipsum Dolor Site Amet 🚀',
+          name: generateRandomName(),
+          message: makeRandomMessage(20) + "🚀",
         }),
       );
     }, 2000);
@@ -25,7 +26,7 @@ const LiveChat = () => {
   }, []);
 
   return (
-    <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
+    <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll'>
       {chatMessages.map((chatMessage, index) => (
         <ChatMessage key={index} name={chatMessage.name} message={chatMessage.message} />
       ))}

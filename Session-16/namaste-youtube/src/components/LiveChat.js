052619488e5371fa7e6294fd94b27577bd27d474
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-15 22:39:06 +0530

    Developed an individual's message in the live chat.

--- Session-16/namaste-youtube/src/components/LiveChat.js
+++ Session-16/namaste-youtube/src/components/LiveChat.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import ChatMessage from './ChatMessage';
 
 const LiveChat = () => {
-  return <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>LiveChat</div>;
+  return (
+    <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
+      <ChatMessage
+        name='Nidhi Gadge'
+        message='This is Namaste React Live! 🙏'
+      />
+      <ChatMessage
+        name='Nidhi Gadge'
+        message='This is Namaste React Live! 🙏'
+      />
+      <ChatMessage
+        name='Nidhi Gadge'
+        message='This is Namaste React Live! 🙏'
+      />
+      <ChatMessage
+        name='Nidhi Gadge'
+        message='This is Namaste React Live! 🙏'
+      />
+      <ChatMessage
+        name='Nidhi Gadge'
+        message='This is Namaste React Live! 🙏'
+      />
+    </div>
+  );
 };
 
 export default LiveChat;

0af1eb0d36d024e7ba638619c36175883962dbf3
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-16 14:37:08 +0530

    Developed a chat input box for users to chat live.

--- Session-16/namaste-youtube/src/components/LiveChat.js
+++ Session-16/namaste-youtube/src/components/LiveChat.js
@@ -17,7 +17,7 @@ const LiveChat = () => {
       dispatch(
         addMessage({
           name: generateRandomName(),
-          message: makeRandomMessage(20) + "🚀",
+          message: makeRandomMessage(20) + '🚀',
         }),
       );
     }, 500);
@@ -26,11 +26,21 @@ const LiveChat = () => {
   }, []);
 
   return (
-    <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse'>
-      {chatMessages.map((chatMessage, index) => (
-        <ChatMessage key={index} name={chatMessage.name} message={chatMessage.message} />
-      ))}
-    </div>
+    <>
+      <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse'>
+        {chatMessages.map((chatMessage, index) => (
+          <ChatMessage
+            key={index}
+            name={chatMessage.name}
+            message={chatMessage.message}
+          />
+        ))}
+      </div>
+      <div className='w-full p-2 ml-2 border border-black'>
+        <input className='border border-blue-600 w-5/6' type='text'></input>
+        <button className='px-2 mx-4 bg-green-100'>Send</button>
+      </div>
+    </>
   );
 };
 

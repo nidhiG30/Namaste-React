c02a38e9ae759264855ac732c187f0e3279030a4
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-16 15:29:14 +0530

    Commented console log.

--- Session-16/namaste-youtube/src/components/Head.js
+++ Session-16/namaste-youtube/src/components/Head.js
@@ -28,7 +28,7 @@ const Head = () => {
   }, [searchQuery]);
 
   const getSearchSuggestions = async () => {
-    console.log('API Call - ' + searchQuery);
+    // console.log('API Call - ' + searchQuery);
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
     // console.log(json[1]);
